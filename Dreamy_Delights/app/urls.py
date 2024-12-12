46c59059f5fa7612efa570a6a614e232cfab2a5b
AswathiVV <aswathi.v.v.916@gmail.com>
2024-12-12 10:44:56 +0530

    DD

---
+++ Dreamy_Delights/app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.home),
+    path('shop_login',views.shop_login),
+    path('register',views.register),
+    path('logout',views.shop_logout),
+# ------------------Admin--------------------------------
+    path('shop_home',views.shop_home),
+    path('add_cupcake',views.add_cupcake),
+    path('cupcake',views.cupcake),
+
+
+# #------------------------------------- User--------------------------------------------------------------
+    path('user_home',views.user_home),
+
+    ]
