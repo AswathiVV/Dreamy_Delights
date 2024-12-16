2f6e65f91b5a088bc30b87420512b9281a79c5a8
AswathiVV <aswathi.v.v.916@gmail.com>
2024-12-16 07:52:25 +0530

    DreamyDelights

---
+++ Dreamy_Delights/Dreamy_Delights/app/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class CupCake(models.Model):
+    id=models.TextField(primary_key=True)
+    name=models.CharField(max_length=100)
+    price=models.IntegerField()
+    img=models.FileField()
+    category=models.TextField()
+    quantity=models.IntegerField()
+    description=models.TextField()
+
+class LayerCake(models.Model):
+    id=models.TextField(primary_key=True)
+    name=models.CharField(max_length=100)
+    price=models.IntegerField()
+    img=models.FileField()
+    category=models.TextField()
+    colour=models.TextField()
+    quantity=models.IntegerField()
+    description=models.TextField()  
+
+class OneTierCake(models.Model):
+    id=models.TextField(primary_key=True)
+    name=models.CharField(max_length=100)
+    price=models.IntegerField()
+    img=models.FileField()
+    category=models.TextField()
+    colour=models.TextField()
+    quantity=models.IntegerField()
+    description=models.TextField() 
+
+class TwoTierCake(models.Model):
+    id=models.TextField(primary_key=True)
+    name=models.CharField(max_length=100)
+    price=models.IntegerField()
+    img=models.FileField()
+    category=models.TextField()
+    colour=models.TextField()
+    quantity=models.IntegerField()
+    description=models.TextField() 
+
