537e1e8df4696e46525e8cc350087dd29579dac0
AswathiVV <aswathiVV916@gmail.com>
2025-02-01 21:04:43 +0530

    DD

--- Dreamy_Delights/Dreamy_Delights/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(CupCake)
-admin.site.register(LayerCake)
-admin.site.register(OneTierCake)
-admin.site.register(TwoTierCake)
